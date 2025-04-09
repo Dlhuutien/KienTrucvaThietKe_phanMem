adae08281828167897803e8cfcdbba621f636859
21040631-NguyenPhiVu <zozotrong123456@gmail.com>
2025-04-09 14:35:16 +0700

    Complete hien thi danh sach mua hang, them mua hang

--- order_service/src/main/java/iuh/fit/se/models/dtos/PurchaseDetailDTO.java
+++ order_service/src/main/java/iuh/fit/se/models/dtos/PurchaseDetailDTO.java
@@ -7,6 +7,13 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -14,8 +21,22 @@ import java.time.LocalDateTime;
 public class PurchaseDetailDTO {
     protected int id;
     private LocalDateTime createdTime;
+    @NotNull(message = "Số lượng không được để trống")
+    @Positive(message = "Số lượng phải là số dương")
+    @Min(value = 1, message = "Số lượng phải lớn hơn 0")
     private int quantity;
+    @NotNull(message = "Giá mua không được để trống")
+    @Positive(message = "Giá mua phải là số dương")
+    @Min(value = 1, message = "Giá mua phải lớn hơn 0")
     private BigDecimal purchasePrice;
-    private int providerId;
-    private int productId;
+    @NotNull(message = "Giá bán không được để trống")
+    @Positive(message = "Giá bán phải là số dương")
+    @Min(value = 1, message = "Giá bán phải lớn hơn 0")
+    private BigDecimal salePrice;
+    // private int providerId;
+    // private int productId;
+    @NotNull(message = "Sản phẩm không được để trống")
+    private String productName;
+    @NotNull(message = "Nhà cung cấp không được để trống")
+    private String providerName;
 }
