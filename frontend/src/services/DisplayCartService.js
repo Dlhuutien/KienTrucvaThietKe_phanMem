43a6c667aa0ac25f80c42e4d1e0a15c037d1c3cb
Dlhuutien <Danglehuutien000@gmail.com>
2025-04-12 12:39:27 +0700

    fixbug shopping

--- frontend/src/services/DisplayCartService.js
+++ frontend/src/services/DisplayCartService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const CART_API_URL = "http://localhost:8000/cart";
-const PRODUCT_API_URL = "http://localhost:8000/api/products";
+const CART_API_URL = "http://localhost:8082/cart";
+const PRODUCT_API_URL = "http://localhost:5000/api/products";
 
 // Lấy cart
 export const getCartByUserId = async (userId) => {
--- order_service/src/main/java/iuh/fit/se/OrderServiceApplication.java
+++ order_service/src/main/java/iuh/fit/se/OrderServiceApplication.java
@@ -40,7 +40,7 @@ public class OrderServiceApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:5173", "http://localhost:3000")
+						.allowedOrigins("http://localhost:5173")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
 						.allowCredentials(true);
