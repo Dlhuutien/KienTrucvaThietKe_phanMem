aea71e50d421367aa4be9aa6d8ee4cf84d7e18cb
Dlhuutien <Danglehuutien000@gmail.com>
2025-04-16 14:18:17 +0700

    build eureka

--- provider_service/src/main/java/iuh/fit/se/ProviderServiceApplication.java
+++ provider_service/src/main/java/iuh/fit/se/ProviderServiceApplication.java
@@ -3,9 +3,11 @@ package iuh.fit.se;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProviderServiceApplication {
 
 	public static void main(String[] args) {
