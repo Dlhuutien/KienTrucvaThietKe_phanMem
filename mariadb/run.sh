9180527479f460b6ebcc21e01168b4dd8153bfe6
Dlhuutien <Danglehuutien000@gmail.com>
2025-05-19 14:49:58 +0700

    deloy eureka

--- mariadb/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Khởi động MariaDB (dưới user mặc định là mysql, không phải root)
-/usr/local/bin/docker-entrypoint.sh mysqld &
-
-# Chạy HTTP server giả để Render không kill service vì không có HTTP response
-python3 -m http.server 3308
