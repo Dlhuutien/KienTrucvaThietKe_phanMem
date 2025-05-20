6885a092b2029978225cce0339d294c50423a216
Dlhuutien <Danglehuutien000@gmail.com>
2025-05-20 17:02:16 +0700

    fix deploy user

--- API-Gateway/src/main/java/iuh/fit/se/configs/CorsGlobalConfiguration.java
+++ API-Gateway/src/main/java/iuh/fit/se/configs/CorsGlobalConfiguration.java
@@ -1,27 +1,27 @@
-//package iuh.fit.se.configs;
-//
-//import java.util.Arrays;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.reactive.CorsWebFilter;
-//import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-//
-//@Configuration
-//public class CorsGlobalConfiguration {
-//
-//    @Bean
-//    public CorsWebFilter corsWebFilter() {
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
-//        config.setAllowedOrigins(Arrays.asList("https://admin-git-deloy-dlhuutiens-projects.vercel.app", "https://temgfrontend-git-deloy-dlhuutiens-projects.vercel.app")); 
-//        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-//        config.setAllowedHeaders(Arrays.asList("*"));
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", config);
-//
-//        return new CorsWebFilter(source);
-//    }
-//}
+package iuh.fit.se.configs;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsGlobalConfiguration {
+
+   @Bean
+   public CorsWebFilter corsWebFilter() {
+       CorsConfiguration config = new CorsConfiguration();
+       config.setAllowCredentials(true);
+       config.setAllowedOrigins(Arrays.asList("https://admin-git-deloy-dlhuutiens-projects.vercel.app", "https://temgfrontend-git-deloy-dlhuutiens-projects.vercel.app")); 
+       config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+       config.setAllowedHeaders(Arrays.asList("*"));
+
+       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+       source.registerCorsConfiguration("/**", config);
+
+       return new CorsWebFilter(source);
+   }
+}
