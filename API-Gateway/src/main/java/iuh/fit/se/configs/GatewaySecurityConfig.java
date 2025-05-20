f1a5cde0fe4841be27b433f86372524fbde9cef0
Dlhuutien <Danglehuutien000@gmail.com>
2025-05-20 22:27:51 +0700

    configs gateway

--- API-Gateway/src/main/java/iuh/fit/se/configs/GatewaySecurityConfig.java
+++ API-Gateway/src/main/java/iuh/fit/se/configs/GatewaySecurityConfig.java
@@ -2,17 +2,16 @@ package iuh.fit.se.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class GatewaySecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-          .csrf().disable()
-          .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.csrf().disable()
+            .authorizeExchange(exchange -> exchange.anyExchange().permitAll());
         return http.build();
     }
 }
