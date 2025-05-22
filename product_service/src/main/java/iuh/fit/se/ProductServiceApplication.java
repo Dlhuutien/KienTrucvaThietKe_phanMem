1833c2f5a4486e61538b6a17f723e5c882274347
Dlhuutien <Danglehuutien000@gmail.com>
2025-05-23 00:19:52 +0700

    merge branch main

--- product_service/src/main/java/iuh/fit/se/ProductServiceApplication.java
+++ product_service/src/main/java/iuh/fit/se/ProductServiceApplication.java
@@ -11,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCaching
+@EnableCaching
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
